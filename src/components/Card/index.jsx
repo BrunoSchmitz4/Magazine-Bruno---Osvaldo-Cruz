8a7d46575ec2167e0e0c1affbb85c993ba91b9b9
Bruno Schmitz <85579710+BrunoSchmitz4@users.noreply.github.com>
2026-04-14 13:13:28 -0300

    First commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import style from './Card.module.css';
+
+function Card({ title, desc, price, img }) {
+    return <>
+        <div className={style.card}>
+            <img className={style.img} src={img} alt={title} />
+            <h4 className={style.title}>{title}</h4>
+            <p className={style.description}>{desc}</p>
+            <p className={style.price}>R$ {price.toFixed(2).replace('.', ',')}</p>
+        </div>
+    </>
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Header />
+    <Card
+        title="Produto 1"
+        desc="Descrição do Produto 1"
+        price={99.9}
+        img={"https://media-cdn.bnn.in.th/234729/iPhone_14_Plus_Blue_PDP_Image_Position-1A_Blue_3-square_medium.jpg"}
+      />
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
