9123d94861d452b7623eedd6570cfd39de1ce041
Bruno Schmitz <85579710+BrunoSchmitz4@users.noreply.github.com>
2026-04-15 15:47:10 -0300

    Update
    Created Dinamic List

--- src/components/ConteudoPrincipal/index.jsx
+++ src/components/ConteudoPrincipal/index.jsx
@@ -7,16 +7,19 @@ function ConteudoPrincipal() {
         {id: 1, nome: "Mouse Gamer", preco: 12.90, em_estoque: true, img: ""},
         {id: 2, nome: "Teclado Gamer", preco: 118.90, em_estoque: false, img: ""},
         {id: 3, nome: "Monitor Gamer", preco: 590.90, em_estoque: true, img: ""},
+        
     ]
     return <>
-        {lista_produtos.map((produto) => (
-            <Card key={produto.id} 
-            title={produto.nome} 
-            price={produto.preco} 
-            desc={produto.em_estoque}
-            img={produto.img}
-            />
-        ))}
+        <div className={style.container}>
+            {lista_produtos.map((produto) => (
+                <Card key={produto.id} 
+                title={produto.nome} 
+                price={produto.preco} 
+                desc={produto.em_estoque}
+                img={produto.img}
+                />
+            ))}
+        </div>
     </>
 }
 
